7935816cf57318ead245f75f453954dfa7b18f33
ahmedhh33 <ahmedhh3633@gmail.com>
2023-10-28 18:04:40 +0400

    change main

--- JS/main.js
+++ JS/main.js
@@ -51,8 +51,8 @@ const createRow = (book) => {
   const actionCell = document.createElement("td");
   const removeButton = document.createElement("button");
   removeButton.classList.add("remove-button");
-  removeButton.textContent = "Remove";
-  removeButton.addEventListener("click", () => removeBook(book.b_ID));
+  removeButton.textContent = "Borrow";
+  removeButton.addEventListener("click", () => borrowBook(book.b_ID));
   actionCell.appendChild(removeButton);
   row.appendChild(actionCell);
 
@@ -65,29 +65,35 @@ const addCell = (row, text) => {
   row.appendChild(cell);
 };
 
-const removeBook = (bookId) => {
-  const confirmation = confirm("Are you sure you want to remove this book?");
+const borrowBook = (bookId) => {
+  const confirmation = confirm("Are you sure you want to borrow this book?");
   if (confirmation) {
-    deleteBook(bookId)
-      .then((response) => {
-        if (response.ok) {
-          const row = document.getElementById(`book-${bookId}`);
-          row.parentNode.removeChild(row);
-        } else {
-          throw new Error("Failed to delete the book");
-        }
-      })
-      .catch((error) => {
-        console.log("An error occurred while deleting the book:", error);
-      });
-  }
-};
+    window.location.href = "Borrow.html";
+    function handleBorrowngForm(event) {
+      event.preventDefault();
+      const accountNumber = document.getElementById("accountNumber").value;
+      const accountHolderId = document.getElementById("accountHolderId").value;
+      const patronId = document.getElementById("patronId").value;
 
-const deleteBook = (bookId) => {
-  const deleteUrl = `${apiUrl}?ID=${bookId}`;
-  return fetch(deleteUrl, {
-    method: "DELETE",
-  });
+      // Call the borrowBook API endpoint here with the necessary data
+      const borrowUrl = `https://localhost:7211/api/BorrowingTransactions?patronId=${patronId}&bookId=${bookId}&AccountNumber=${accountNumber}&AccountHolderID=${accountHolderId}`;
+      fetch(borrowUrl, {
+        method: "POST",
+      })
+        .then((response) => {
+          if (response.ok) {
+            alert("Book borrowed successfully");
+            window.location.href = "main.html";
+            // Update the UI or handle any necessary tasks after successful borrowing
+          } else {
+            throw new Error("Failed to borrow the book");
+          }
+        })
+        .catch((error) => {
+          console.log("An error occurred while borrowing the book:", error);
+        });
+    }
+  }
 };
 
 searchBooks();
--- JS/test2.js
+++ JS/test2.js
@@ -48,6 +48,13 @@ const createRow = (book) => {
   addCell(row, book.publication_year);
   addCell(row, book.is_Available ? "True" : "False");
 
+  const actionCell = document.createElement("td");
+  const removeButton = document.createElement("button");
+  removeButton.classList.add("remove-button");
+  removeButton.textContent = "Borrow";
+  removeButton.addEventListener("click", () => borrowBook(book.b_ID));
+  actionCell.appendChild(removeButton);
+  row.appendChild(actionCell);
 
   return row;
 };
@@ -56,6 +63,38 @@ const addCell = (row, text) => {
   const cell = document.createElement("td");
   cell.textContent = text;
   row.appendChild(cell);
+};
+
+const borrowBook = (bookId) => {
+  const confirmation = confirm("Are you sure you want to borrow this book?");
+  if (confirmation) {
+    window.location.href = "Borrow.html";
+    function handleBorrowngForm(event)  {
+      event.preventDefault();
+      const accountNumber = document.getElementById("accountNumber").value;
+    const accountHolderId = document.getElementById("accountHolderId").value;
+    const patronId = document.getElementById("patronId").value;
+
+
+      // Call the borrowBook API endpoint here with the necessary data
+      const borrowUrl = `https://localhost:7211/api/BorrowingTransactions?patronId=${patronId}&bookId=${bookId}&AccountNumber=${accountNumber}&AccountHolderID=${accountHolderId}`;
+      fetch(borrowUrl, {
+        method: "POST",
+      })
+        .then((response) => {
+          if (response.ok) {
+            alert("Book borrowed successfully");
+            window.location.href = "test2.html";
+            // Update the UI or handle any necessary tasks after successful borrowing
+          } else {
+            throw new Error("Failed to borrow the book");
+          }
+        })
+        .catch((error) => {
+          console.log("An error occurred while borrowing the book:", error);
+        });
+    };
+  }
 };
 
 searchBooks();
